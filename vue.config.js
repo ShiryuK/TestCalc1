326318f53524f2869ce525bb2648609d3dcd6189
ShiryuK <shiryu2000note@yahoo.co.jp>
2023-12-02 16:26:28 +0900

    Add vue.config.js and build files for GitHub Pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+// vue.config.js
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-vue-project/'  // このパスはリポジトリ名に基づいて変更してください
+    : '/'
+}
